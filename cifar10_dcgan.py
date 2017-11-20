e250350ed484df6baa181a224b896aaf1392f114
We1lesszz <631253076@qq.com>
2017-11-20 16:39:25 +0800

    cifar10_gan

---
+++ cifar10_dcgan.py
@@ -0,0 +1,160 @@
+import tensorflow as tf
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+from load_cifar10 import load_CIFAR10
+
+def get_session():
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    session = tf.Session(config=config)
+    return session
+
+# def get_CIFAR10_data(num_training=49000, num_validation=1000, num_test=10000):
+#     cifar10_dir = 'data/cifar-10-batches-py'
+#     X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
+#     # Subsample the data
+#     mask = range(num_training, num_training + num_validation)
+#     X_val = X_train[mask]
+#     y_val = y_train[mask]
+#     mask = range(num_training)
+#     X_train = X_train[mask]
+#     y_train = y_train[mask]
+#     mask = range(num_test)
+#     X_test = X_test[mask]
+#     y_test = y_test[mask]
+#     # Normalize the data: subtract the mean image
+#     mean_image = np.mean(X_train, axis=0)
+#     X_train -= mean_image
+#     X_val -= mean_image
+#     X_test -= mean_image
+#     return X_train, y_train, X_val, y_val, X_test, y_test
+
+cifar10_dir = 'data/cifar-10-batches-py'
+X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
+mean_image = np.mean(X_train, axis=0)
+X_train -= mean_image
+X_test -= mean_image
+
+# Invoke the above function to get our data.
+print('Train data shape: ', X_train.shape)
+print('Train labels shape: ', y_train.shape)
+print('Test data shape: ', X_test.shape)
+print('Test labels shape: ', y_test.shape)
+
+
+def leaky_relu(x, alpha=0.01):
+    return tf.maximum(x, alpha * x)
+
+
+def sample_noise(batch_size, dim):
+    # return tf.random_uniform([batch_size, dim], minval=-1, maxval=1)
+    return tf.random_normal([batch_size, dim])
+
+def discriminator(x):
+    with tf.variable_scope("discriminator"):
+        x = tf.layers.conv2d(x, filters=32, kernel_size=5, strides=1, activation=leaky_relu)
+        x = tf.layers.max_pooling2d(x, pool_size=2, strides=2)
+        x = tf.layers.conv2d(x, filters=64, kernel_size=5, strides=1, activation=leaky_relu)
+        x = tf.layers.max_pooling2d(x, pool_size=2, strides=2)
+        x = tf.reshape(x, [-1, 3200])
+        x = tf.layers.dense(x, units=4 * 4 * 64, activation=leaky_relu)
+        x = tf.layers.dense(x, units=1)
+        return x
+
+
+def generator(z):
+    with tf.variable_scope("generator"):
+        z = tf.layers.dense(x, units=1024)
+        z = tf.layers.batch_normalization(z, training=is_training)
+        z = tf.nn.relu(z)
+        z = tf.layers.dense(z, units=7 * 7 * 128)
+        z = tf.layers.batch_normalization(z, training=is_training)
+        z = tf.nn.relu(z)
+        z = tf.reshape(z, [-1, 8, 8, 128])
+        z = tf.layers.conv2d_transpose(z, filters=64, kernel_size=4, strides=2, padding='SAME')
+        z = tf.layers.batch_normalization(z, training=is_training)
+        z = tf.nn.relu(z)
+        z = tf.layers.conv2d_transpose(z, filters=3, kernel_size=4, strides=2, padding='SAME',
+                                         activation=tf.tanh)
+        return z
+
+tf.reset_default_graph()
+
+
+def get_solvers(learning_rate=1e-3, beta1=0.5):
+    D_solver = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=beta1)
+    G_solver = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=beta1)
+    pass
+    return D_solver, G_solver
+
+# our noise dimension
+noise_dim = 96
+show_every=250
+print_every=50
+batch_size=8
+num_epoch=100
+
+# placeholders for images from the training dataset
+x = tf.placeholder(tf.float32, [None, 32, 32, 3])
+z = sample_noise(batch_size, noise_dim)
+is_training = tf.placeholder(tf.bool)
+# generated images
+G_sample = generator(z)
+
+with tf.variable_scope("") as scope:
+    #scale images to be -1 to 1
+    logits_real = discriminator(x)
+    # Re-use discriminator weights on new inputs
+    scope.reuse_variables()
+    logits_fake = discriminator(G_sample)
+
+
+
+ones = tf.ones_like(logits_fake)
+zeros = tf.zeros_like(logits_fake)
+G_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits_fake, labels=ones))
+D_loss = 0
+D_loss += tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits_real, labels=ones))
+D_loss += tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits_fake, labels=zeros))
+
+
+# Get the list of variables for the discriminator and generator
+D_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,'discriminator')
+G_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,'generator')
+
+D_solver,G_solver = get_solvers()
+
+D_train_step = D_solver.minimize(D_loss, var_list=D_vars)
+G_train_step = G_solver.minimize(G_loss, var_list=G_vars)
+D_extra_step = tf.get_collection(tf.GraphKeys.UPDATE_OPS,'discriminator')
+G_extra_step = tf.get_collection(tf.GraphKeys.UPDATE_OPS,'generator')
+tf.summary.scalar('D_loss', D_loss)
+tf.summary.scalar('G_loss', G_loss)
+summ = tf.summary.merge_all()
+
+
+
+with get_session() as sess:
+    sess.run(tf.global_variables_initializer())
+    writer = tf.summary.FileWriter('./graphs', sess.graph)
+
+    for epoch in range(num_epoch):
+        total_batch = int(len(X_train) / batch_size)
+        for i in range(total_batch):
+            batch_x = X_train[i:i + batch_size]
+            _,  __= sess.run([D_train_step,G_train_step], feed_dict={x: batch_x,is_training: True})
+            D_loss_curr,  G_loss_curr, summm = sess.run([D_loss, G_loss, summ], feed_dict={x: batch_x,is_training: True})
+            # _, G_loss_curr = sess.run([G_train_step, G_loss], feed_dict={is_training: True})
+            writer.add_summary(summm)
+        # print loss every so often.
+        # We want to make sure D_loss doesn't go to 0
+        print('Epoch: {}, D: {:.4}, G:{:.4}'.format(epoch, D_loss_curr, G_loss_curr))
+
+    print('Final images')
+    samples = sess.run(G_sample,feed_dict={is_training: False})
+    for h in range(10):
+        fig = samples[h]
+        plt.savefig('cifarfinal'+str(h)+'.jpg')
